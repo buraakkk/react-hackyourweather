df972f8ed00c672a6482c55101b949691050e27a
Burak ISIK <50028862+buraakkk@users.noreply.github.com>
2021-06-04 23:47:36 +0200

    Create App.js

---
+++ project/src/App.js
@@ -0,0 +1,27 @@
+import React, { useState } from "react";
+import Weather from "./Weather";
+import CityForecast from "./CityForecast";
+import "./App.css";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+
+const App = () => {
+  const [cities, setCities] = useState([]);
+  return (
+    <Router>
+      <Switch>
+        <Route
+          path="/"
+          exact
+          component={(routeProps) => {
+            return (
+              <Weather {...routeProps} cities={cities} setCities={setCities} />
+            );
+          }}
+        />
+        <Route path="/:id" component={CityForecast} />
+      </Switch>
+    </Router>
+  );
+};
+
+export default App;
